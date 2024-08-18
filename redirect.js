645a7d20255221d51347efce6b68b493c4667266
Kristy98755 <121288707+Kristy98755@users.noreply.github.com>
2024-08-19 00:37:57 +0600

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,17 +1,14 @@
-// Определяем тип устройства
-const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const url = new URL(window.location.href);
+const pathParts = url.pathname.split('/');
+const lastPart = pathParts[pathParts.length - 1];
 
-// Получаем текущий URL
-const currentUrl = window.location.href;
-
-// Проверяем, является ли URL мобильной версией
-const isMobileVersion = currentUrl.includes('/m.startpage');
-
-// Проверка соответствия типа устройства и версии сайта
-if (isMobile && !isMobileVersion) {
-    // Если устройство мобильное, но версия сайта не мобильная, перенаправляем на мобильную версию
-    window.location.href = currentUrl.replace('/startpage', '/m.startpage');
-} else if (!isMobile && isMobileVersion) {
-    // Если устройство не мобильное, но версия сайта мобильная, перенаправляем на полную версию
-    window.location.href = currentUrl.replace('/m.startpage', '/startpage');
+if (isMobile && !lastPart.startsWith('m.')) {
+    pathParts[pathParts.length - 1] = 'm.' + lastPart;
+    url.pathname = pathParts.join('/');
+    window.location.href = url.href;
+} else if (!isMobile && lastPart.startsWith('m.')) {
+    pathParts[pathParts.length - 1] = lastPart.substring(2);
+    url.pathname = pathParts.join('/');
+    window.location.href = url.href;
 }
